4113777c8594e68fcc2c3d59ad15f47971dad638
Amir Alian <amircup2006@gmail.com>
2020-11-23 11:16:52 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+echo "11111"
+CHANGED_FILES=$(git diff --name-only --diff-filter=AM ${PREVIOUS_COMMIT} ${CURRENT_COMMIT})
+
+echo "22222"
+CHANGED_FILES_PHP=$(find ${CHANGED_FILES} -type f -regex "^.*\(\.php\|\.phtml\)$")
+
+echo "333333"
+CHANGED_CORE_FILES=$(find ${CHANGED_FILES} -type f -regex "^\(app/code/core\|app/design/frontend/base\|app/design/adminhtml/base\)/.+$")
+
+echo ${CHANGED_CORE_FILES}
+for file in ${CHANGED_CORE_FILES}; do		
+
+
+    echo "5555"
+    RESULTS="Unchangable file is changed: ${file}"
+    echo "\n${RESULTS}\n"	
+    ERROR=1	
+    echo "6666"
+done	
+exit "${ERROR}"
