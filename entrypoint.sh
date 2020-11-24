1c27037ac29585d28db36901a0bb28e96a7dc8c4
Amir Alian <amircup2006@gmail.com>
2020-11-24 16:21:31 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,12 @@ echo ${MODIFIED_FILES}
 echo "**************** changed files php"
 echo ${CHANGED_FILES_PHP}
 
-CHANGED_CORE_FILES=$(find ${MODIFIED_FILES} -type f -regex "^\(app/code/core\|app/design/frontend/base\|app/design/adminhtml/base\|app/code/community\)/.+$")
+CHANGED_CORE_FILES=$(find ${MODIFIED_FILES} -type f -regex "^\(app/code/core\|app/design/frontend/base\|app/design/adminhtml/base\|app/code/community\)/.+$" 2>/dev/null)
+
+echo "*************CHANGED_CORE_FILES"
+echo ${CHANGED_CORE_FILES}
+
+
 
 for file in ${CHANGED_CORE_FILES}; do		
     RESULTS="Unchangable file is changed: ${file}"
