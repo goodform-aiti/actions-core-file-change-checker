61e34f1ef1dc6e3c08862396046580fc95834abe
Amir Alian <amircup2006@gmail.com>
2020-11-24 14:01:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -e
 ERROR=0
-CHANGED_FILES=$(git diff --name-only --diff-filter=AM main...${CURRENT_COMMIT})
+echo " ************** MODIFIED FILES"
+echo ${MODIFIED_FILES}
 
-echo ${CHANGED_FILES}
+echo "**************** changed files php"
+echo ${CHANGED_FILES_PHP}
 
-CHANGED_FILES_PHP=$(find ${CHANGED_FILES} -type f -regex "^.*\(\.php\|\.phtml\)$")
-CHANGED_CORE_FILES=$(find ${CHANGED_FILES} -type f -regex "^\(app/code/core\|app/design/frontend/base\|app/design/adminhtml/base\|app/code/community\)/.+$")
+CHANGED_CORE_FILES=$(find ${MODIFIED_FILES} -type f -regex "^\(app/code/core\|app/design/frontend/base\|app/design/adminhtml/base\|app/code/community\)/.+$")
 
 for file in ${CHANGED_CORE_FILES}; do		
     RESULTS="Unchangable file is changed: ${file}"
