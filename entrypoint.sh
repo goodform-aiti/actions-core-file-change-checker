0703c394b8f63b593bff800d7cbdabc1324baa3b
Amir Alian <amircup2006@gmail.com>
2020-11-25 15:27:26 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,34 +6,23 @@ echo ${MODIFIED_FILES}
 echo " *****************************"
 
 
-paths=(${MODIFIED_FILES//,/ })
-for i in "${!paths[@]}"
+PATHS=(${MODIFIED_FILES//,/ })
+for i in "${!PATHS[@]}"
 do
-#     if [[ ${paths[i]} =~ ^app\/code\/core\/(.+)$ ]] ; then
-#       echo "Core file is changed: ${paths[i]}"
-#       ERROR=1
-#     fi
-    
-    
-    
-    while read -r local_file && [ ! -z "$local_file" ];	
-    do	
-        echo "heeey"
-        #all local files must be found in changed files	
-        if ! grep "^$local_file\$" <<< "$MODIFIED_FILES" >/dev/null; then	
-            echo "Not found changes in local file '$local_file' when core file changed." >&2	
-            exit_code=102	
-        fi	
 
-    done < <(grep -P '^app/code/core/.' <<< "$MODIFIED_FILES" | sed --expression='s/^app\/code\/core/app\/code\/local/g' | xargs -r -l -d'\n')
-    
-    
-    
-    
-    
-    
-    if [[ ${paths[i]} =~ ^app\/code\/community\/(.+)$ ]] ; then
-        echo "Community file is changed: ${paths[i]}"
+    for i in "${!PATHS[@]}"
+    do
+        if [[ ${PATHS[i]} =~ ^app\/code\/core\/(.+)$ ]] ; then
+          LOCAL_FILE=$(echo ${PATHS[i]} | sed  --expression='s/^app\/code\/core/app\/code\/local/g')
+          if [[ ! " ${PATHS[@]} " =~ " ${LOCAL_FILE} " ]]; then
+              echo "Not found changes in local file '$LOCAL_FILE' when core file changed." >&2
+              ERROR=1
+          fi
+        fi
+    done
+        
+    if [[ ${PATHS[i]} =~ ^app\/code\/community\/(.+)$ ]] ; then
+        echo "Community file is changed: ${PATHS[i]}"
         ERROR=1
     fi
 done
