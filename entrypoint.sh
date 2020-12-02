38a17f9410d425c865f38b126b187816fde08a9a
Amir Alian <amircup2006@gmail.com>
2020-12-02 11:05:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,20 @@ printf ${MODIFIED_FILES}
 printf "\n*****************************\n"
 
 
-PATHS=(${MODIFIED_FILES//\\n / })
-for i in "${!PATHS[@]}"
-do
 
-    if [[ ${PATHS[i]} =~ ^app\/code\/core\/(.+)$ ]] ; then
-      LOCAL_FILE=$(echo ${PATHS[i]} | sed  --expression='s/^app\/code\/core/app\/code\/local/g')
+PATHS=$(printf ${MODIFIED_FILES} | tr \\n '\n')
+echo "$PATHS" | while read PATH
+do
+    if [[ ${PATH} =~ ^app\/code\/core\/(.+)$ ]] ; then
+      LOCAL_FILE=$(echo ${PATH} | sed  --expression='s/^app\/code\/core/app\/code\/local/g')
       if [[ ! " ${PATHS[@]} " =~ " ${LOCAL_FILE} " ]]; then
           echo "Not found changes in local file '$LOCAL_FILE' when core file changed." >&2
           ERROR=1
       fi
     fi
         
-    if [[ ${PATHS[i]} =~ ^app\/code\/community\/(.+)$ ]] ; then
-        echo "Community file is changed: ${PATHS[i]}"
+    if [[ ${PATH} =~ ^app\/code\/community\/(.+)$ ]] ; then
+        echo "Community file is changed: ${PATH}"
         ERROR=1
     fi
 done
