9177b5cc55b1252e1a14a0a20589c63b83b22b0e
Amir Alian <amircup2006@gmail.com>
2020-12-02 12:04:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,6 +31,14 @@ do
 done < <(grep -P '^app/code/core/.' <<< "$PATHS" | sed --expression='s/^app\/code\/core/app\/code\/local/g')
 
 
+community_overrides=$(grep -P '^app/code/community/.' <<< "$PATHS" | sed --expression='s/^app\/code\/community/app\/code\/core/g')
+
+if [ ! -z "$community_overrides" ]; then
+    echo "Found overrides in 'app/code/community/'! If you need override core file, you must use local scope, not community. List of found files:" >&2
+    echo "$community_overrides" >&2
+    exit_code=103
+fi
+
 
 
 exit "${ERROR}"
