94a7a0e7813c4a97e763f15004ceb6634504efe7
Amir Alian <amircup2006@gmail.com>
2020-12-04 10:56:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-ERROR=0
 
 echo " ************** MODIFIED FILES"
 printf ${MODIFIED_FILES}
@@ -14,7 +13,7 @@ do
     #all local files must be found in changed files
     if ! grep "^$local_file\$" <<< "$PATHS" >/dev/null; then
         echo "Not found changes in local file '$local_file' when core file changed." >&2
-        ERROR=102
+        exit 102
     fi
     
 
@@ -28,7 +27,7 @@ do
     #all core files must be found in changed files
     if ! grep "^$core_file\$" <<< "$PATHS" >/dev/null; then
         echo "Not found changes in core file '$core_file' when local file changed." >&2
-        ERROR=102
+        exit 102
     fi
     
 
@@ -49,15 +48,15 @@ echo "$PATHS" | while read PATH ; do
     fi
     if [[ ${PATH} =~ ^(lib\/phpseclib|lib\/Zend|lib/PEAR)\/.+$ ]] ; then
         echo "Holy code changed: ${PATH}"
-        ERROR=101
+        exit 101
     fi
     if [[ ${PATH} =~ ^app\/design\/frontend\/base\/(.+)$ ]] ; then
         echo "Holy code changed: ${PATH}"
-        ERROR=101
+        exit 101
     fi
     if [[ ${PATH} =~ ^app\/code\/local\/Varien\/(.+)$ ]] ; then
         echo "Holy code changed: ${PATH}"
-        ERROR=101
+        exit 101
     fi
 done
 
@@ -68,9 +67,6 @@ echo "$community_overrides" | while read CORE_FILE ; do
     if [[ -f $CORE_FILE ]]
     then
         echo "Found overrides in 'app/code/community/'! If you need override core file, you must use local scope, not community. The file is: $CORE_FILE"
-        ERROR=103
+        exit 103
     fi
 done
-
-
-exit "${ERROR}"
